5224ea9baed399e8efd20d9cf8f66e79625675e3
Devjyoti Singha <devjyoti@Devjyotis-MacBook-Air.local>
2024-11-19 06:15:17 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+require('dotenv').config()
+const dbConfig = require("./config/dbConfig");
+const port=process.env.PORT || 3000;
+app.use(express.json());
+
+const usersRoute = require("./routes/usersRoute");
+const busesRoute = require("./routes/busesRoute");
+const bookingsRoute = require("./routes/bookingRoute");
+
+app.use(express.json({ limit: '10mb' }));  // You can adjust the size as needed
+
+app.use("/api/users", usersRoute);
+app.use("/api/buses", busesRoute);
+app.use("/api/bookings", bookingsRoute);
+
+
+app.listen(port, () => console.log(`Node server listening on port ${port}!`));
